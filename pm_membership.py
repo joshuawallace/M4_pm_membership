00064da2d89bdb306ff604281d194ccab87b1df6
joshuawallace <joshuawallace800@gmail.com>
2018-10-23 16:17:16 -0400

    got reading in and fitting working

--- pm_membership.py
+++ pm_membership.py
@@ -11,13 +11,98 @@ If you use this code, please cite (fill in information after publication)
 While this code is focused on M4, it can be modified to work on other
 clusters or moving groups, and may work with no other alternation than 
 pointing the code to the correct input file.
+
+The code is segmented into functions for reading in and fitting the catalog
+and reading in and using the fitted model.  If you would like to use the
+model I already fitted, you will only need the latter functionality.
+
+Usage: pm_membership.py input_catalog_filename output_pickle_filename
+
+     input_catalog_filename: the filename for the input catalog, assumed to
+          be a VOTable from the Gaia data archive or formatted similarly
+
+     output_pickle_filename: the filename for the pickle file that will store
+          the fitted Gaussian mixture model for later use
+
 """
 
 from __future__ import absolute_import, division, print_function
 import sys, pickle
+import numpy as np
 from astropy.io.votable import parse_single_table
 import matplotlib.pyplot as plt
 from sklearn.mixture import GaussianMixture as GM
 
 
-# First, read in the desired input and output file names
+def read_catalog_in_and_fit(input_filename, output_filename, mag_cutoff=19.3,
+                            initial_means=[[-12.5,-19], [-3,-4]],
+                            random_state=546):
+    """
+    input_filename - the input catalog, assume to be a VOTable and assumed
+         to have similar format as the Gaia DR2 source catalog
+    output_filename - the name for the output pickle file, which will store
+         the fitted model
+    mag_cutoff - the dimmest (maximum) G-band magnitude for stars to use 
+         in the model fit
+    initial_means - the initial guesses for the means of the two Gaussian
+         components.  Random initializations (the default) lead to random
+         assignments: sometimes the first component is what gets fit to 
+         the cluster proper motions, other times its the second component.
+         For consistency, the default initial_means provided here is intended
+         so that the first component fits the cluster proper motions, which
+         have a mean value around the default value shown above.
+    random_state - here we have provided a fixed seed value for the random
+         number generator for consistency across runs.  Changing this value
+         should have little to no effect on the final fit.
+
+    Returns the fitted model
+    """
+
+    # Read in the catalog
+    table = parse_single_table(input_filename)
+    catalog_data = table.array
+
+    # Set up the Gaussian mixture model
+    gm_model = GM(n_components=2,max_iter=300,means_init=initial_means,
+                  random_state=random_state)
+    
+    # Mask out catalog entries without measured proper motions and dimmer
+    # than the cutoff magnitude
+    pm_mask = (catalog_data['phot_g_mean_mag'] < mag_cutoff) & \
+        (~np.isnan(catalog_data['pmra'])) & (~np.isnan(catalog_data['pmdec']))
+    
+    # Extract the data to fit and fit it
+    data_to_fit = [item for item in zip(catalog_data['pmra'][pm_mask],
+                                        catalog_data['pmdec'][pm_mask])]
+    gm_model.fit(data_to_fit)
+
+    # Dump the fitted model to a pickle file, and also return it
+    with open(output_filename,"wb") as f:
+        pickle.dump(gm_model,f)
+    return gm_model
+
+
+def read_model_in_and_use(model_filename,proper_motions):
+    """
+    model_filename - the name for the output pickle file, which stores
+         the fitted model
+    proper_motions - list/array of proper motions to be fed into the model,
+         of format ()
+    """
+    pass
+
+
+if __name__ == "__main__":
+    # First, read in the desired input and output filenames
+    if len(sys.argv) != 3:
+        print ("Needed command line arguments: input_catalog_filename " +\
+                   "output_pickle_filename")
+        raise RuntimeError("Wrong number of command line arguments.  Got " +\
+                               str(len(sys.argv)-1) + " instead of 2")
+
+    input_filename = sys.argv[1]
+    output_filename = sys.argv[2]
+
+
+    # Next, read in the catalog and fit membership model
+    fitted_model = read_catalog_in_and_fit(input_filename, output_filename)
