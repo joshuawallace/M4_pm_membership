71d41b7cc422a705972eb41dc1894a44099cbd64
joshuawallace <joshuawallace800@gmail.com>
2018-11-07 18:35:12 -0500

    pm_membership.py now creates plain text catalog file

--- pm_membership.py
+++ pm_membership.py
@@ -16,7 +16,11 @@ The code is segmented into functions for reading in and fitting the catalog
 and reading in and using the fitted model.  If you would like to use the
 model I already fitted, you will only need the latter functionality.
 
-Usage: pm_membership.py input_catalog_filename output_pickle_filename
+This code has some redunancies/unnecessary steps for a full end-to-end
+calculation, but these were included to provide e.g. examples of how 
+to read in the fitted model from a pickle file and use the contents.
+
+Usage: pm_membership.py  input_catalog_filename  output_pickle_filename
 
      input_catalog_filename: the filename for the input catalog, assumed to
           be a VOTable from the Gaia data archive or formatted similarly
@@ -38,6 +42,8 @@ def read_catalog_in_and_fit(input_filename, output_filename, mag_cutoff=19.3,
                             initial_means=[[-12.5,-19], [-3,-4]],
                             random_state=546):
     """
+    Read in the Gaia data and fit the Gaussian mixture model
+
     input_filename - the input catalog, assume to be a VOTable and assumed
          to have similar format as the Gaia DR2 source catalog
     output_filename - the name for the output pickle file, which will store
@@ -55,7 +61,7 @@ def read_catalog_in_and_fit(input_filename, output_filename, mag_cutoff=19.3,
          number generator for consistency across runs.  Changing this value
          should have little to no effect on the final fit.
 
-    Returns the fitted model and the proper motions
+    Returns the fitted model and the read-in catalog data
     """
 
     # Read in the catalog
@@ -68,53 +74,93 @@ def read_catalog_in_and_fit(input_filename, output_filename, mag_cutoff=19.3,
     
     # Mask out catalog entries without measured proper motions and dimmer
     # than the cutoff magnitude
-    pm_mask = (catalog_data['phot_g_mean_mag'] < mag_cutoff) & \
+    pm_mask_fitting = (catalog_data['phot_g_mean_mag'] < mag_cutoff) & \
         (~np.isnan(catalog_data['pmra'])) & (~np.isnan(catalog_data['pmdec']))
     
     # Extract the data to fit and fit it
-    data_to_fit = [item for item in zip(catalog_data['pmra'][pm_mask],
-                                        catalog_data['pmdec'][pm_mask])]
+    data_to_fit = [item for item in zip(catalog_data['pmra'][pm_mask_fitting],
+                                        catalog_data['pmdec'][pm_mask_fitting])]
     gm_model.fit(data_to_fit)
 
     # Dump the fitted model to a pickle file, and also return it
     with open(output_filename,"wb") as f:
         pickle.dump(gm_model,f)
-    return (gm_model, (catalog_data['pmra'][pm_mask],
-                       catalog_data['pmdec'][pm_mask]))
+    return (gm_model, catalog_data)
 
 
-def read_model_in_and_use(model_filename,proper_motions):
+def read_model_in_and_use(model_filename, catalog_data):
     """
+    Read the model in from the given pickle file and use it
+
     model_filename - the name for the output pickle file, which stores
          the fitted model
-    proper_motions - list/array of proper motions to be fed into the model,
-         of format [ [pm_ra_1, pm_dec_1], [pm_ra_2, pm_dec_2], ...]
+    catalog_data - the read-in data for the objects, assumed to be in
+         similar format as the Gaia DR2 data.  See the 
+         read_catalog_in_and_fit() function for an example for how to
+         read in the catalog
     """
 
-    # Read in 
+    # Read in the fitted model
     with open(model_filename,"rb") as f:
         fitted_model = pickle.load(f)
     
-    # Output the membership probability from the proper motions
-    prob = fitted_model.predict_proba(proper_motions)
+    # From the catalog data, get the proper motions and calculate probability
+    pm_mask_calculating = (~np.isnan(catalog_data['pmra'])) &\
+        (~np.isnan(catalog_data['pmdec']))
+    data_to_calc = [item for item in zip(catalog_data['pmra'][pm_mask_calculating],
+                                        catalog_data['pmdec'][pm_mask_calculating])]
+    prob = fitted_model.predict_proba(data_to_calc)
+    prob_first_comp = [item[0] for item in prob] # Just the first component
 
-    # And return just the first component's membership probability
-    return np.array([item[0] for item in prob])
+    # Now create a dictionary matching ID to membership probability
+    prob_dict = {}
+    for i in range(len(catalog_data['source_id'][pm_mask_calculating])):
+        prob_dict[catalog_data['source_id'][i]] = prob_first_comp[i]
+
+    # And return the probability dict
+    return prob_dict
 
 
-def plot_up(proper_motions,membership_probabilities,
+def plot_up(catalog_data,membership_probabilities,
             plot_filename="gmm_fit.png"):
     """
-    proper_motions - the proper motions [pm_ra_list, pm_dec_list]
-    membership_probabilities - the membership probabilities associated
-         with the proper motions above
+    Show the membership probabilities graphically
+
+    catalog_data - the read-in data for the objects, assumed to be in
+         similar format as the Gaia DR2 data.  See the 
+         read_catalog_in_and_fit() function for an example for how to
+         read in the catalog
+    membership_probabilities - a dictionary relating membership 
+         probabilities to source IDs
     plot_filename - name of the file for the output plot
     """
+
+    """
+    # Prepare the data
+    pmra = []
+    pmdec = []
+    probs = []
+    for i in range(len(catalog_data['source_id'])):
+        if catalog_data['source_id'][i] in membership_probabilities.keys():
+            pmra.append(catalog_data['pmra'][i])
+            pmdec.append(catalog_data['pmdec'][i])
+            probs.append(membership_probabilities[catalog_data['source_id'][i]])
+    """
+
+    # Extract all the relevant information, assuming that the keys to 
+    # membership_probabilities dictionary is the most complete list of what
+    # we want to plot up
+    pmra = catalog_data['pmra'][np.isin(catalog_data['source_id'],
+                                        membership_probabilities.keys())]
+    pmdec = catalog_data['pmdec'][np.isin(catalog_data['source_id'],
+                                        membership_probabilities.keys())]
+    source_ids_ordered = catalog_data['source_id'][np.isin(catalog_data['source_id'],
+                                        membership_probabilities.keys())]
+    probs = [membership_probabilities[ID] for ID in source_ids_ordered]
+
     
     # Plot up the points and make colorbar
-    sc = plt.scatter(proper_motions[0],proper_motions[1],
-                     c=membership_probabilities,cmap='brg',
-                     s=1,alpha=.6)
+    sc = plt.scatter(pmra,pmdec,c=probs,cmap='brg',s=1,alpha=.6)
     cbar = plt.colorbar(sc,label='Star cluster membership probability')
 
     # Add labels and save
@@ -125,6 +171,54 @@ def plot_up(proper_motions,membership_probabilities,
     plt.tight_layout()
     plt.savefig(plot_filename,dpi=300)
     plt.close()
+    
+
+def produce_catalog(catalog_data,membership_probabilities,
+                    output_catalog_filename="membership_catalog.txt"):
+    """
+    After calculating the probability memberships, write the 
+    final output membership catalog.
+
+    catalog_data - the read-in data for the objects, assumed to be in
+         similar format as the Gaia DR2 data.  See the 
+         read_catalog_in_and_fit() function for an example for how to
+         read in the catalog
+    membership_probabilities - a dictionary relating membership 
+         probabilities to source IDs
+
+
+    """
+    source_ids_ordered = catalog_data['source_id'][np.isin(catalog_data['source_id'],
+                                        membership_probabilities.keys())]
+    G_mag = catalog_data['phot_g_mean_mag'][np.isin(catalog_data['source_id'],
+                                        membership_probabilities.keys())]
+    ra = catalog_data['ra'][np.isin(catalog_data['source_id'],
+                                        membership_probabilities.keys())]
+    dec = catalog_data['dec'][np.isin(catalog_data['source_id'],
+                                        membership_probabilities.keys())]
+    ra_err = catalog_data['ra_error'][np.isin(catalog_data['source_id'],
+                                        membership_probabilities.keys())]
+    dec_err = catalog_data['dec_error'][np.isin(catalog_data['source_id'],
+                                        membership_probabilities.keys())]
+    pmra = catalog_data['pmra'][np.isin(catalog_data['source_id'],
+                                        membership_probabilities.keys())]
+    pmdec = catalog_data['pmdec'][np.isin(catalog_data['source_id'],
+                                        membership_probabilities.keys())]
+    pmra_err = catalog_data['pmra_error'][np.isin(catalog_data['source_id'],
+                                        membership_probabilities.keys())]
+    pmdec_err = catalog_data['pmdec_error'][np.isin(catalog_data['source_id'],
+                                        membership_probabilities.keys())]
+    probs = [membership_probabilities[ID] for ID in source_ids_ordered]
+
+    with open(output_catalog_filename,"w") as f:
+        f.write("# Gaia_DR2_ID   Gaia_G_mag  memb_prob  RA    RA_err   dec  dec_err " +\
+                    "  pm_RA   pm_RA_err   pm_dec   pm_dec_err\n")
+        formatter = "%20s  %20s  %20s  %20s  %20s  %20s  %20s  %20s  %20s  %20s  %20s\n"
+        for i in range(len(source_ids_ordered)):
+            f.write(formatter % (source_ids_ordered[i], G_mag[i],
+                                 probs[i], ra[i], ra_err[i], dec[i], dec_err[i],
+                                 pmra[i], pmra_err[i], pmdec[i], pmdec_err[i]))
+       
     
 
 
@@ -141,8 +235,8 @@ if __name__ == "__main__":
 
 
     # Next, read in the catalog and fit membership model
-    fitted_model, proper_motions = read_catalog_in_and_fit(input_filename, 
-                                                           output_filename)
+    fitted_model, catalog_data = read_catalog_in_and_fit(input_filename, 
+                                                         output_filename)
     # We can use the fitted_model directly:
     sample_proper_motions = [ [-12.5,-19],
                               [-3,-4],
@@ -153,13 +247,11 @@ if __name__ == "__main__":
     print([item[0] for item in probability_output]) # Just the first component
 
     # Or we can use the function above:
-    function_probability_output = read_model_in_and_use(output_filename,
-                                                        sample_proper_motions)
-    print(function_probability_output)
+    full_prob_output = read_model_in_and_use(output_filename,
+                                             catalog_data)
 
     # Let's plot it up
-    proper_motion_tofit = [item for item in zip(proper_motions[0],
-                                                proper_motions[1])]
-    full_prob_output = read_model_in_and_use(output_filename,
-                                             proper_motion_tofit)
-    plot_up(proper_motions,full_prob_output)
+    plot_up(catalog_data,full_prob_output)
+
+    # And now write out the plain text catalog
+    produce_catalog(catalog_data,full_prob_output)
